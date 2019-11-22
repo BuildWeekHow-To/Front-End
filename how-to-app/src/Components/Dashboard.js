67be4ab19d39e64ea23656bd1ed47bbf0b236f60
Dijah Deen <dijah@iams-MacBook-Pro.local>
2019-11-21 21:04:53 -0500

    this is a final commit

--- how-to-app/src/Components/Dashboard.js
+++ how-to-app/src/Components/Dashboard.js
@@ -1,16 +1,26 @@
 import React, { useEffect, useState } from 'react'
 import Styled from 'styled-components';
+import axios from "axios";
 import { axiosWithoutAuth } from './utils/axiosWithAuth';
-
-
+import { HowToCard } from './HowToCard';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+// const CardsContainer = Styled.div`
+// display: flex;
+// flex-wrap: wrap;
+// `
+// const IndividualCards= Styled.div`
+// border: 1px solid black;
+// border-radius: 1rem;
+// width: 25%;
+// margin: 1rem;
+// `
 export default class Dashboard extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             howtos: []
         }
     }
-
     getData = () => {
         axiosWithoutAuth()
             .get('howtos')
@@ -20,24 +30,41 @@ export default class Dashboard extends React.Component {
             })
             .catch(err => console.log(err))
     }
-
     componentDidMount() {
         this.getData();
     }
-
+    deleteCard = (id) => {
+        axiosWithoutAuth()
+            .delete(`https://build-week-how-to.herokuapp.com/api/howtos/${id}`)
+            .then(res => {
+                console.log(res);
+                this.getData();
+            })
+            .catch(err => console.log(err.response));
+    }
     render() {
-        const howtos = this.state.howtos;
         return (
-            <CardsContainer className='CardsContainer' >
-                {howtos.map(item => (
-                    <IndividualCards key={item.id} className='IndividualCards' >
-                        <H2> {item.name} </H2>
-                        <p> {item.desc} </p>
-
-                    </IndividualCards>
-                ))}
-            </CardsContainer>
+            <div>
+                <Route
+                    render={props => {
+                        return <HowToCard {...props} howtos={this.state.howtos} deleteCard={this.deleteCard} />
+                    }}
+                />
+            </div>
         )
     }
-
 }
+{/* <CardsContainer className='CardsContainer' >
+                {howtos.map(item => (
+                    <IndividualCards key={item.id} className='IndividualCards' >
+                        <h2> {item.name} </h2>
+                        <p> {item.desc} </p>
+                        <button onClick={()=> {this.props.history.push(`/update-howtos/${item.id}`)}} >
+                            Edit
+                        </button>
+                        <button  onClick={()=>this.deleteCard(item.id)}>
+                            Delete
+                        </button>
+                    </IndividualCards>
+                ))}
+            </CardsContainer> */}
